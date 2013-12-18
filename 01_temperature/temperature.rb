35c4782517c55235325ae39860be1b1b3f2d41ba
Giustino Borzacchiello <giustinob@gmail.com>
2013-12-18 17:58:10 +0100

    First commit

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(fdeg)
+	return (fdeg - 32) * (5.0/9.0)	
+end
+
+def ctof(cdeg)
+	return cdeg * (9.0 / 5.0) + 32
+end
---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,19 @@
+def add(a, b)
+	a+b
+end
+
+def subtract(a, b)
+	a-b
+end
+
+def sum(arr)
+	sum = 0
+	arr.each { |v| sum += v }
+	sum
+end
+
+def multiply(*numbers)
+	prod = 1
+	numbers.each { |v| prod *= v}
+	prod
+end
